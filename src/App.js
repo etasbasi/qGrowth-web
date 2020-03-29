e4223b043d23d4ed6e6fc3292a856f94e1b12efa
Enes Tasbasi <enestasbasi@outlook.com>
2020-03-29 00:43:23 -0400

    Fix error message

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
-import React from "react";
-import "./App.css";
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.scss";
+
+import Login from "./components/Login";
 
 function App() {
-  return <div className="App"></div>;
+  useEffect(() => {
+    const isAuthenticated = (() => {
+      return localStorage.getItem("token") !== null;
+    })();
+
+    if (isAuthenticated) {
+      window.location.replace("/");
+    }
+  }, []);
+
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route exact path="/login" component={Login} />
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
